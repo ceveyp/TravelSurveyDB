7ad71d800fec3d813b62cd620df1b9e172adee84
Philip Cevey <philip_cevey@outlook.com>
2026-01-24 00:26:14 -0500

    Add new taxonomy mappings

--- taxonomy_migration/migrator.py
+++ taxonomy_migration/migrator.py
@@ -3,6 +3,14 @@ from typing import Dict
 
 from sqlmodel import SQLModel
 
+from db.models import (
+    QuestionDisabilityMap,
+    QuestionTravelCategoryMap,
+    Question,
+    QuestionCategoryMap,
+    Disability,
+    TravelCategory
+)
 from db.orm import Session
 from taxonomy_migration.readers.data_loader import TaxonomyDataLoader
 from taxonomy_migration.readers.spreadsheet import TaxonomySpreadsheetReader
@@ -21,6 +29,15 @@ class TaxonomyMigrator:
         self._loader = TaxonomyDataLoader(self._db)
         self._spreadsheet_taxonomy: TaxonomyData | None = None
         self._db_taxonomy: TaxonomyData | None = None
+        self._new_entity_counters = {
+            "questions": 0,
+            "disabilities": 0,
+            "travel_categories": 0,
+            "medical_categories": 0,
+            "question_disability_maps": 0,
+            "question_travel_category_maps": 0,
+            "question_medical_category_maps": 0
+        }
 
     def _add_new_entities(self, spreadsheet_entities_map: Dict[str, SQLModel], db_entities_map: Dict[str, SQLModel]):
         """Add new entities from spreadsheet to DB"""
@@ -29,7 +46,69 @@ class TaxonomyMigrator:
                 continue
             logger.info(f"New {type(entity)} found: {entity.model_dump()}")
             self._db.add(entity)
-            self._db_taxonomy.question_key_entity_map[entity_key] = entity
+            db_entities_map[entity_key] = entity
+            if isinstance(entity, Disability):
+                new_counter_key = 'disabilities'
+            elif isinstance(entity, Question):
+                new_counter_key = 'questions'
+            elif isinstance(entity, TravelCategory):
+                new_counter_key = 'travel_categories'
+            else:
+                new_counter_key = 'medical_categories'
+            self._new_entity_counters[new_counter_key] += 1
+
+    def _add_new_question_disability_maps(self, question: Question, taxonomy_maps: Dict):
+        """Add new question-disability maps from spreadsheet to DB"""
+        question_key = question.question_key
+        question_key_taxonomy_map = self._db_taxonomy.question_key_taxonomy_map
+        existing_question_disabilities = question_key_taxonomy_map[question_key]["disabilities"]
+        for disability_key, disability_map in taxonomy_maps["disabilities"].items():
+            if not disability_key in existing_question_disabilities:
+                qdm = QuestionDisabilityMap()
+                qdm.question = question
+                qdm.disability = self._db_taxonomy.disability_key_entity_map[disability_key]
+                qdm.reason = disability_map.reason
+                self._db.add(qdm)
+                self._new_entity_counters["question_disability_maps"] += 1
+
+    def _add_new_question_travel_category_maps(self, question: Question, taxonomy_maps: Dict):
+        """Add new question-travel category maps from spreadsheet to DB"""
+        question_key = question.question_key
+        question_key_taxonomy_map = self._db_taxonomy.question_key_taxonomy_map
+        existing_question_travel_categories = question_key_taxonomy_map[question_key]["travel_categories"]
+        for travel_category_key, travel_category_map in taxonomy_maps["travel_categories"].items():
+            if not travel_category_key in existing_question_travel_categories:
+                q_tcm = QuestionTravelCategoryMap()
+                q_tcm.question = question
+                q_tcm.travel_category = self._db_taxonomy.travel_category_key_entity_map[travel_category_key]
+                self._db.add(q_tcm)
+                self._new_entity_counters["question_travel_category_maps"] += 1
+
+    def _add_new_question_medical_category_maps(self, question: Question, taxonomy_maps: Dict):
+        """Add new question-medical category maps from spreadsheet to DB"""
+        question_key = question.question_key
+        question_key_taxonomy_map = self._db_taxonomy.question_key_taxonomy_map
+        existing_question_medical_categories = question_key_taxonomy_map[question_key]["medical_categories"]
+        for medical_category_key, medical_category_map in taxonomy_maps["medical_categories"].items():
+            if not medical_category_key in existing_question_medical_categories:
+                q_mcm = QuestionCategoryMap()
+                q_mcm.question = question
+                q_mcm.category = self._db_taxonomy.medical_category_key_entity_map[medical_category_key]
+                self._db.add(q_mcm)
+                self._new_entity_counters["question_medical_category_maps"] += 1
+
+    def _display_add_new_taxonomies_output(self):
+        print("--------------------------------")
+        print("\nFinished adding new taxonomies:")
+        print(f"\n\tAdded {self._new_entity_counters["questions"]} Questions")
+        print(f"\n\tAdded {self._new_entity_counters["disabilities"]} Disabilities")
+        print(f"\n\tAdded {self._new_entity_counters["travel_categories"]} Travel Categories")
+        print(f"\n\tAdded {self._new_entity_counters["medical_categories"]} Medical Categories\n")
+
+        print(f"\n\tAdded {self._new_entity_counters["question_disability_maps"]} Question > Disability maps")
+        print(f"\n\tAdded {self._new_entity_counters["question_travel_category_maps"]} Question > Travel Category maps")
+        print(
+            f"\n\tAdded {self._new_entity_counters["question_medical_category_maps"]} Question > Medical Category maps")
 
     def _add_new_taxonomy(self):
         """Add new taxonomy from spreadsheet to DB"""
@@ -44,12 +123,15 @@ class TaxonomyMigrator:
         self._add_new_entities(st.medical_category_key_entity_map, dt.medical_category_key_entity_map)
         self._add_new_entities(st.travel_category_key_entity_map, dt.travel_category_key_entity_map)
 
-        """for question_key, taxonomy_maps in self._spreadsheet_taxonomy.question_key_taxonomy_map.items():
-            for disability_key, disability_map in taxonomy_maps["disabilities"]:
-                existing_question_disabilities = self._db_taxonomy.question_key_taxonomy_map[question_key][
-                    "disabilities"]
-                if not disability_key in existing_question_disabilities:
-                    self._db.add(disability_map)"""
+        for question_key, taxonomy_maps in st.question_key_taxonomy_map.items():
+            question = dt.question_key_entity_map[question_key]
+
+            # Add all new taxonomy mappings
+            self._add_new_question_disability_maps(question, taxonomy_maps)
+            self._add_new_question_travel_category_maps(question, taxonomy_maps)
+            self._add_new_question_medical_category_maps(question, taxonomy_maps)
+
+        self._display_add_new_taxonomies_output()
 
     def migrate(self):
         self._spreadsheet_taxonomy = self._reader.read()
--- taxonomy_migration/readers/data_loader.py
+++ taxonomy_migration/readers/data_loader.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 from typing import Dict, List, Type
 
-from sqlalchemy.orm import joinedload
 from sqlmodel import SQLModel
 
 from db.models import (
@@ -33,7 +32,10 @@ class TaxonomyDataLoader:
     def _fetch_taxonomy_key_entity_map(self, entity_type: Type[SQLModel], entity_map: Dict[str, SQLModel]):
         entities = self._db.query(entity_type).all()
         for entity in entities:
-            entity_map[entity.key] = entity
+            if isinstance(entity, Question):
+                entity_map[entity.question_key] = entity
+            else:
+                entity_map[entity.key] = entity
 
     def load(self) -> TaxonomyData:
         logger.debug("Loading taxonomy data from DB")
@@ -44,53 +46,32 @@ class TaxonomyDataLoader:
         self._fetch_taxonomy_key_entity_map(MedicalCategory, self._medical_category_key_entity_map)
         self._fetch_taxonomy_key_entity_map(TravelCategory, self._travel_category_key_entity_map)
 
+        tx_map = self._question_key_taxonomy_map
+
         # Map question key to medical category
-        medical_category_maps: List[QuestionCategoryMap] = (
-            self._db.query(QuestionCategoryMap)
-            .options(
-                joinedload(QuestionCategoryMap.question),
-                joinedload(QuestionCategoryMap.category)
-            )
-            .all()
-        )
+        medical_category_maps: List[QuestionCategoryMap] = self._db.query(QuestionCategoryMap).all()
         for medical_category_map in medical_category_maps:
             question_key = medical_category_map.question.question_key
             medical_category_key = medical_category_map.category.key
-            self._question_key_taxonomy_map[question_key]["medical_categories"][
-                medical_category_key] = medical_category_map
+            tx_map[question_key]["medical_categories"][medical_category_key] = medical_category_map
 
         # Map question key to travel category
-        travel_category_maps: List[QuestionTravelCategoryMap] = (
-            self._db.query(QuestionTravelCategoryMap)
-            .options(
-                joinedload(QuestionTravelCategoryMap.question),
-                joinedload(QuestionTravelCategoryMap.travel_category)
-            )
-            .all()
-        )
+        travel_category_maps: List[QuestionTravelCategoryMap] = self._db.query(QuestionTravelCategoryMap).all()
         for travel_category_map in travel_category_maps:
             question_key = travel_category_map.question.question_key
             travel_category_key = travel_category_map.travel_category.key
-            self._question_key_taxonomy_map[question_key]["travel_categories"][
-                travel_category_key] = travel_category_map
+            tx_map[question_key]["travel_categories"][travel_category_key] = travel_category_map
 
         # Map question key to disability
-        disability_maps: List[QuestionDisabilityMap] = (
-            self._db.query(QuestionDisabilityMap)
-            .options(
-                joinedload(QuestionDisabilityMap.question),
-                joinedload(QuestionDisabilityMap.disability)
-                .joinedload(Disability.market_data)
-            )
-            .all()
-        )
+        disability_maps: List[QuestionDisabilityMap] = self._db.query(QuestionDisabilityMap).all()
         for disability_map in disability_maps:
             question_key = disability_map.question.question_key
             disability_key = disability_map.disability.key
-            self._question_key_taxonomy_map[question_key]["disabilities"][disability_key] = disability_map
+            tx_map[question_key]["disabilities"][disability_key] = disability_map
+
         logger.debug("Finished loading taxonomy data from DB")
         return TaxonomyData(
-            question_key_entity_map=self._question_key_taxonomy_map,
+            question_key_entity_map=self._question_key_entity_map,
             question_key_taxonomy_map=self._question_key_taxonomy_map,
             disability_key_entity_map=self._disability_key_entity_map,
             medical_category_key_entity_map=self._medical_category_key_entity_map,
